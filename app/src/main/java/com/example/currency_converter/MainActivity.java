cc71f0d77c999283fed4e31d774e5a3bf1c5b65a
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-02-04 21:31:57 +0200

    tozbeet

--- app/src/main/java/com/example/currency_converter/MainActivity.java
+++ app/src/main/java/com/example/currency_converter/MainActivity.java
@@ -10,23 +10,27 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     EditText usd;
     EditText lbp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        usd=(EditText)findViewById(R.id.usd);
-        lbp=(EditText)findViewById(R.id.lbp);
+        usd = (EditText) findViewById(R.id.usd);
+        lbp = (EditText) findViewById(R.id.lbp);
     }
 
-    public void amount(View v){
-        if(usd.getText().toString()!=null && lbp.getText().toString()==null){
-            int result=0;
-            result=22000*Integer.parseInt(usd.getText().toString());
+    public void check_amount(View v) {
+        String usd1 = usd.getText().toString();
+        String lbp1 = lbp.getText().toString();
+        if (usd1 != null && lbp1 == null) {
+            int result = 0;
+            result = 22000 * Integer.parseInt(usd1);
+            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
             lbp.setText(result);
-        }else{
-            int result1=0;
-            result1=Integer.parseInt(lbp.getText().toString())/22000;
-            lbp.setText(result1);
+        } else {
+            int result1 = 0;
+            result1 = Integer.parseInt(lbp1) / 22000;
+            usd.setText(result1);
 
         }
 
