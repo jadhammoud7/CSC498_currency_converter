f312156e72fb6c7ac4fb1cd51817b8af4b02faf5
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-02-04 20:38:47 +0200

    added onclick function

--- app/src/main/java/com/example/currency_converter/MainActivity.java
+++ app/src/main/java/com/example/currency_converter/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.currency_converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText usd;
+    EditText lbp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usd=(EditText)findViewById(R.id.usd);
+        lbp=(EditText)findViewById(R.id.lbp);
+    }
+
+    public void amount(View v){
+        if(usd.getText().toString()!=null && lbp.getText().toString()==null){
+            int result=0;
+            result=22000*Integer.parseInt(usd.getText().toString());
+            lbp.setText(result);
+        }else{
+            int result1=0;
+            result1=Integer.parseInt(lbp.getText().toString())/22000;
+            lbp.setText(result1);
+
+        }
+
     }
 }
